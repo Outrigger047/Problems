a3038892632633090203428629861ff461a295df
Andrew Wyshak <andrew.wyshak@gmail.com>
2023-11-15 10:11:07 -0500

    Checking for null from reflection methods

--- Program.cs
+++ Program.cs
@@ -7,25 +7,34 @@ namespace Problems
         public static void Main(string[] args)
         {
             // Build a collection of all methods
-            var methods = Type.GetType("Problems.Program").GetMethods(BindingFlags.NonPublic | BindingFlags.Static).ToList();
+            var type = Type.GetType("Problems.Program");
 
-            // Print list of all pairs; ask for user input to select method to run
-            for (int i = 1; i < methods.Count + 1; i += 1)
+            if (type != null)
             {
-                Console.WriteLine($"  ({i}): {methods[i - 1].Name}");
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Static).ToList();
+
+                // Print list of all pairs; ask for user input to select method to run
+                for (int i = 1; i < methods.Count + 1; i += 1)
+                {
+                    Console.WriteLine($"  ({i}): {methods[i - 1].Name}");
+                }
+
+                Console.Write("Choose a method number to run: ");
+                int.TryParse(Console.ReadLine(), out var userSelection);
+
+                // Dynamically invoke method based on user input
+                try
+                {
+                    methods[userSelection - 1].Invoke(null, null);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Bad method number");
+                }
             }
-
-            Console.Write("Choose a method number to run: ");
-            int.TryParse(Console.ReadLine(), out var userSelection);
-
-            // Dynamically invoke method based on user input
-            try
+            else
             {
-                methods[userSelection - 1].Invoke(null, null);
-            }
-            catch(Exception)
-            {
-                Console.WriteLine("Bad method number");
+                Console.WriteLine("Unable to get methods");
             }
         }
     }
